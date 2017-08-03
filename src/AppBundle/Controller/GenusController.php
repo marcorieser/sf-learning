3938a58bf2ed09f4e04108f6b0b5d9365ddb85b7
Marco Rieser <mrieser@dachcom.ch>
2017-08-03 10:33:57 +0200

    Video 5

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -3,15 +3,25 @@
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
-class GenusController
+class GenusController extends Controller
 {
     /**
      * @Route("/genus/{genusName}")
+     * @param $genusName
+     *
+     * @return Response
      */
     public function showAction($genusName)
     {
-        return new Response('The genus: ' . $genusName);
+        $templating = $this->container->get('templating');
+
+        $html = $templating->render('genus/show.html.twig', [
+            'name' => $genusName
+        ]);
+
+        return new Response($html);
     }
 }
