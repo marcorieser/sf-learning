5afeaae0abeebfa419b0cd0074f368c80616300e
Marco Rieser <mrieser@dachcom.ch>
2017-08-03 16:45:35 +0200

    Relationships 9

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -79,9 +79,10 @@ class GenusController extends Controller
         //            $cache->save($key, $funFact);
         //        }
 
-        $recentNotes = $genus->getNotes()->filter(function (GenusNote $note) {
-            return $note->getCreatedAt() > new \DateTime('-3 months');
-        });
+        //        $recentNotes = $genus->getNotes()->filter(function (GenusNote $note) {
+        //            return $note->getCreatedAt() > new \DateTime('-3 months');
+        //        });
+        $recentNotes = $em->getRepository('AppBundle:GenusNote')->findAllRecentNotesForGenus($genus);
 
         return $this->render('genus/show.html.twig', [
             'genus' => $genus,
