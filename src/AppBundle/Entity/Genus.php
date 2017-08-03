05db931d23631e496cd89bc7428e8d595b99d70b
Marco Rieser <mrieser@dachcom.ch>
2017-08-03 15:26:05 +0200

    Doctrine 11

--- src/AppBundle/Entity/Genus.php
+++ src/AppBundle/Entity/Genus.php
@@ -38,6 +38,11 @@ class Genus
     private $funFact;
 
     /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isPublished;
+
+    /**
      * @return mixed
      */
     public function getName()
@@ -107,6 +112,14 @@ class Genus
     public function getUpdatedAt()
     {
         return new \DateTime('-' . rand(0, 100) . ' days');
+    }
+
+    /**
+     * @param mixed $isPublished
+     */
+    public function setIsPublished($isPublished)
+    {
+        $this->isPublished = $isPublished;
     }
 
 }
