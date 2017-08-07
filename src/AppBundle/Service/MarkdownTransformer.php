d0417f38d437c76608ba3ea383f8659e276e7ae1
Marco Rieser <mrieser@dachcom.ch>
2017-08-07 08:49:13 +0200

    Services 4

--- src/AppBundle/Service/MarkdownTransformer.php
+++ src/AppBundle/Service/MarkdownTransformer.php
@@ -2,17 +2,19 @@
 
 namespace AppBundle\Service;
 
+use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
+
 class MarkdownTransformer
 {
     private $markdownParser;
 
-    public function __construct($markdownParser)
+    public function __construct(MarkdownParserInterface $markdownParser)
     {
         $this->markdownParser = $markdownParser;
     }
 
     public function parse($str)
     {
-        return $this->markdownParser->transform($str);
+        return $this->markdownParser->transformMarkdown($str);
     }
 }
