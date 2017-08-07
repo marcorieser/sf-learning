7bc1f2f3d23de794b8d3c3f7d7977f70f5773f79
Marco Rieser <mrieser@dachcom.ch>
2017-08-07 08:58:55 +0200

    Services 5

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -68,7 +68,7 @@ class GenusController extends Controller
             throw $this->createNotFoundException('No genus found');
         }
 
-        $transformer = new MarkdownTransformer($this->get('markdown.parser'));
+        $transformer = $this->get('app.markdown_transformer');
         $funFact = $transformer->parse($genus->getFunFact());
 
         //        $cache = $this->get('doctrine_cache.providers.my_markdown_cache');
