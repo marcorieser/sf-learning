fdaf8a00f4339de091ce00103b4ed2f31113e830
Marco Rieser <mrieser@dachcom.ch>
2017-08-07 08:45:10 +0200

    Services 3

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\Genus;
 use AppBundle\Entity\GenusNote;
+use AppBundle\Service\MarkdownTransformer;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -67,6 +68,9 @@ class GenusController extends Controller
             throw $this->createNotFoundException('No genus found');
         }
 
+        $transformer = new MarkdownTransformer($this->get('markdown.parser'));
+        $funFact = $transformer->parse($genus->getFunFact());
+
         //        $cache = $this->get('doctrine_cache.providers.my_markdown_cache');
         //
         //        $key = md5($funFact);
@@ -86,6 +90,7 @@ class GenusController extends Controller
 
         return $this->render('genus/show.html.twig', [
             'genus' => $genus,
+            'funFact' => $funFact,
             'recentNoteCount' => count($recentNotes)
         ]);
     }
