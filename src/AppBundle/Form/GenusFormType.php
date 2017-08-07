0943ae5b076a2901d3074b291956de891acdd2a9
Marco Rieser <mrieser@dachcom.ch>
2017-08-07 17:56:13 +0200

    Forms 8

--- src/AppBundle/Form/GenusFormType.php
+++ src/AppBundle/Form/GenusFormType.php
@@ -4,10 +4,10 @@ namespace AppBundle\Form;
 
 use AppBundle\Entity\SubFamily;
 use AppBundle\Repository\SubFamilyRepository;
-use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -16,24 +16,24 @@ class GenusFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
 
-        $builder
-            ->add('name')
-            ->add('subFamily', EntityType::class, [
-                'class' => SubFamily::class,
-                'placeholder' => 'Choose a Sub Family',
-                'query_builder' => function (SubFamilyRepository $repo) {
-                    return $repo->createAlphabeticalQueryBuilder();
-                }
-            ])
-            ->add('speciesCount')
-            ->add('funFact')
-            ->add('isPublished', ChoiceType::class, [
-                'choices' => [
-                    'Yes' => true,
-                    'No' => false
-                ]
-            ])
-            ->add('firstDiscoveredAt');
+        $builder->add('name')->add('subFamily', EntityType::class, [
+            'class' => SubFamily::class,
+            'placeholder' => 'Choose a Sub Family',
+            'query_builder' => function (SubFamilyRepository $repo) {
+                return $repo->createAlphabeticalQueryBuilder();
+            }
+        ])->add('speciesCount')->add('funFact')->add('isPublished', ChoiceType::class, [
+            'choices' => [
+                'Yes' => true,
+                'No' => false
+            ]
+        ])->add('firstDiscoveredAt', DateType::class, [
+            'widget' => 'single_text',
+            'attr' => [
+                'class' => 'js-datepicker'
+            ],
+            'html5' => false
+        ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
